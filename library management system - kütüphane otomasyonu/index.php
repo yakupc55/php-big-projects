4b84762f154a5159732fd931b483137642ff5be6
Diyar Ceran <yakupceran00@gmail.com>
2021-03-06 19:13:33 +0100

    version 1.0 added
    büyük çaplı projemizin ilk kısımlarını aktardık

---
+++ library management system - kütüphane otomasyonu/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Online Library management system</title>
+</head>
+
+<body>
+
+<div class="wrapper">
+
+    <header>
+        <nav>
+            <ul>
+                <li><a href="">HOME</a></li>
+                <li><a href="">BOOKS</a></li>
+                <li><a href="">STUDENT-LOGIN</a></li>
+                <li><a href="">ADMIN_LOGIN</a></li>
+                <li><a href="">FEEDBACK</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section>
+
+    </section>
+
+    <footer>
+
+    </footer>
+
+</div>
+</body>
+</html>
